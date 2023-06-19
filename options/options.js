1c397777a79cd03a74d047d12736c0060226d46a
Crowdbotics <engineering@crowdbotics.com>
2023-06-19 14:40:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rapid-cell-dev-93031",
+  url: "https://rapid-cell-dev-93031.botics.co",
+  api: "https://rapid-cell-dev-93031.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
